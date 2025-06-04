1b9e1dfe63e7da1996a4bba897e79ab3e9e95769
zro2hirocode <saengsaichaiyo@gmail.com>
2025-06-04 10:53:28 +0700

    update file_html css

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// function openLightbox(src) {
-//   const lightbox = document.getElementById("lightbox");
-//   const img = document.getElementById("lightbox-img");
-//   img.src = src;
-//   lightbox.style.display = "flex";
-// }
+function openLightbox(src) {
+  const lightbox = document.getElementById("lightbox");
+  const img = document.getElementById("lightbox-img");
+  img.src = src;
+  lightbox.style.display = "flex";
+}
 
-// function closeLightbox(event) {
-//   if (event.target.id === "lightbox" || event.target.classList.contains("close")) {
-//     document.getElementById("lightbox").style.display = "none";
-//   }
-// }
+function closeLightbox(event) {
+  if (event.target.id === "lightbox" || event.target.classList.contains("close")) {
+    document.getElementById("lightbox").style.display = "none";
+  }
+}
+
+const menuToggle = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
