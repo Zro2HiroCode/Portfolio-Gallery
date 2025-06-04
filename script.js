48d8fbaf6234c8515ecd587e6d4e2e52b8b6c428
zro2hirocode <saengsaichaiyo@gmail.com>
2025-06-04 09:47:27 +0700

    update file_html,css add file_js

---
+++ script.js
@@ -0,0 +1,12 @@
+// function openLightbox(src) {
+//   const lightbox = document.getElementById("lightbox");
+//   const img = document.getElementById("lightbox-img");
+//   img.src = src;
+//   lightbox.style.display = "flex";
+// }
+
+// function closeLightbox(event) {
+//   if (event.target.id === "lightbox" || event.target.classList.contains("close")) {
+//     document.getElementById("lightbox").style.display = "none";
+//   }
+// }
